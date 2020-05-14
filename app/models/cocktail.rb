946b868e6e5637abde55c97d1b731946c13cc7e0
Ilayda Bakare <ilayda.bakare@gmail.com>
2020-05-14 12:58:13 +0100

    Seeded db

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,5 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
-  has_many :ingredients, through: :doses
+  has_many :ingredients, through: :doses # through join table!
   validates :name, presence: true, uniqueness: true
 end
